3808da70f66cfb68b3831087fa98105cd5d45495
fahadjan2 <fahadfawad2006@gmail.com>
2025-09-24 20:33:01 -0700

    Commit

--- code.cpp
+++ code.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+const int COLOR_SIZE = 3;
+
+//Colors class, includes setters and getters for each color value, as well as a print method to print all the values
 class Color {
 private:
     int red;
@@ -26,7 +29,23 @@ public:
     }
 };
 
+//Populating the data and printing
 int main() {
+    Color * colors = new Color[COLOR_SIZE];
     
+    colors[0].setRed(5);
+    colors[0].setGreen(8);
+    colors[0].setBlue(59);
 
+    colors[1].setRed(45);
+    colors[1].setGreen(25);
+    colors[1].setBlue(51);
+
+    colors[2].setRed(57);
+    colors[2].setGreen(85);
+    colors[2].setBlue(95);
+
+    for (int i = 0; i < COLOR_SIZE; i++) {
+        colors[i].print();
+    }
 }
